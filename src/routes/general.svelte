62452961480e618b68c2dcee7cf10fdc8aeaf8b4
Marco Octavio luna Vargas <marco.octavio.luna@gmail.com>
2021-07-13 16:49:23 -0400

    Commit 1; add general.svelte y contact.svelte; en esta parte editamos y agregamos nuevos componentes al header, editamos el Nav.svelte que es donde estan nuestras pestañas

---
+++ src/routes/general.svelte
@@ -0,0 +1,10 @@
+<!--Este archivo es general como  dice su nombre, nos ayudara a manejar las etiquetas que vamos a tener dentro de cada uno de los blogPost-->
+
+<!--Esto es para tener acceso a el head de la pagina web-->
+<svelte:head>
+    <title>General</title>
+</svelte:head>
+
+<div class="General">
+    <h2>General</h2>
+</div>
